c1c46891d87c6fda975b9b47530d8343b37bcd2c
conorbailey90 <conbailey90@gmail.com>
2023-07-06 06:24:43 +0100

    Contact form updated

--- src/app/components/Contact/Contact.jsx
+++ src/app/components/Contact/Contact.jsx
@@ -28,12 +28,13 @@ export default function Contact({children}) {
       let res = await request.json();
       console.log(res)
       alert('Thank you for your email. We will respond shortly.')
+      name.current.value = ''
+      email.current.value = ''
+      message.current.value = ''
     }catch(err){
       alert('Sorry, something went wrong. Please try again.')
-
       console.log(err)
     }
-    
   }
 
   return (
@@ -43,7 +44,6 @@ export default function Contact({children}) {
           <h2>Get in touch</h2> <br />
           <p>For enquiries, please enter your name, email and message using the form below and we will respond to you as soon as possible. </p>
           <input type="hidden" name="_captcha" value="false" /> 
-    
           <input placeholder='Enter name' className={styles.input} ref={name} type="text" name="name" required /> <br />
           <input placeholder='Enter email' className={styles.input} ref={email} type="email" name="email" required /> <br />
           <textarea placeholder='Enter message here.' className={styles.message} ref={message} name="message" id="" cols="30" rows="10"></textarea> <br />
