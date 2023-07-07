9c8c4bbf7384f5b10a7913b048bab3c4efcd974a
conorbailey90 <conbailey90@gmail.com>
2023-07-07 10:55:02 +0100

    Meta data updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 
 import styles from './page.module.css'
+import logo from '../../public/images/logo.png'
 import {Hero, About, Testimonial, Treatments, Contact, Location, Canvas, MobileNav, Separator , Prices, FAQ} from './components/export'
 export const metadata = {
   title: 'FACEWORX',
   description: 'Faceworx anti-wrinkle injections, dermal fillers and skin boosters.',
-  openGraph: { type: "website", url: "https://faceworx.beauty", title: "FACEWORX", description: "Faceworx anti-wrinkle injections, dermal fillers and skin boosters.", siteName: "FACEWORX", image: '/public/images/logo.png'}
+  openGraph: { type: "website", url: "https://faceworx.beauty", title: "FACEWORX", description: "Faceworx anti-wrinkle injections, dermal fillers and skin boosters.", siteName: "FACEWORX", image: logo}
 }
 
 export default async function HomePage() {
