0478c8e5aa94d3024973c70928d5616980bf1b68
Conor Bailey <conbailey90@gmail.com>
2025-05-31 23:54:43 +0100

    Products section css updated

--- src/components/Prices/PriceList/Pricelist.jsx
+++ src/components/Prices/PriceList/Pricelist.jsx
@@ -3,16 +3,19 @@ import styles from './PriceList.module.css'
 function Pricelist({category, index}) {
 
   return (
-    <div className={index % 2 == 0 ? styles.even : styles.odd}>
+    <div className={styles.listItem}>
           <h3 style={{marginBottom: '1rem'}}>{category.catName}</h3>
           <ul>
-            {category.products && category.products.map(product => (
-                <li key={product._id} className={styles.priceLi}>
-                    <div style={{flex: 3}}>
+            {category.products && category.products.map((product, idx) => (
+                <li key={idx} className={styles.priceLi}>
+                    <div className={styles.treatment}>
                         <p>{product.treatment}</p>
                         {product.description && <p className={styles.desc}>{product.description}</p>}
                     </div>
-                    <p style={{flex: 1}} className={styles.price}>{product.price}</p>
+                    <div >
+                      <p  className={styles.price}>{product.price}</p>
+                    </div>
+                  
                 </li>
             ))}
           </ul>
